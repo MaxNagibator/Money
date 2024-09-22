b9e0ca235ff87362ae6106e4cdcc917e779c53ad
Савелий Вотинцев <votintcev1@mail.ru>
2024-09-22 03:42:44 +0300

    Web-проект
    - Добавлен шаблон web-проекта
    - Добавлен GitHub Action для тестирования

---
+++ Money.Web/Program.cs
@@ -0,0 +1,26 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Money.Web;
+using MudBlazor.Services;
+
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(_ => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    // Configure your authentication provider options here.
+    // For more information, see https://aka.ms/blazor-standalone-auth
+    builder.Configuration.Bind("Local", options.ProviderOptions);
+});
+
+builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
+
+await builder.Build().RunAsync();
