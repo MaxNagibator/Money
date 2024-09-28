3131b8ad2a03e837cc02ebb131a7a0c4843c1b08
Mihkail Afanasev <afanasev10056@gmail.com>
2024-09-29 00:50:47 +0300

    добавил базовые классы для валидации

---
+++ frontend/Money.Web/Validators/BaseValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Money.Web.Validators;
+
+public class BaseValidator<T> : AbstractValidator<T>
+{
+    public BaseValidator(Action<IRuleBuilderInitial<T, T>> rule)
+    {
+        rule(RuleFor(x => x));
+    }
+
+    private IEnumerable<string> ValidateValue(T arg)
+    {
+        var result = Validate(arg);
+        if (result.IsValid)
+            return new string[0];
+        return result.Errors.Select(e => e.ErrorMessage);
+    }
+
+    public Func<T, IEnumerable<string>> Validation => ValidateValue;
+}
---
+++ frontend/Money.Web/Validators/Category/CategoryDialogModelFluentValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Money.Web.Validators.Category;
+
+public class CategoryDialogModelFluentValidator : AbstractValidator<Models.Category>
+{
+    
+    public CategoryDialogModelFluentValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .Length(1,40);
+    }
+
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<Models.Category>.CreateWithOptions((Models.Category)model, x => x.IncludeProperties(propertyName)));
+        if (result.IsValid)
+            return Array.Empty<string>();
+        return result.Errors.Select(e => e.ErrorMessage);
+    };
+}
