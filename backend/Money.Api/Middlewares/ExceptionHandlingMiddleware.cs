27aaf50f1fd7c21fc3f117c691744a9f5680cd42
Gritsina Maksim <bob217@mail.ru>
2024-09-30 22:45:55 +0700

    спрятан текст необработанной ошибки

--- backend/Money.Api/Middlewares/ExceptionHandlingMiddleware.cs
+++ backend/Money.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -34,7 +34,7 @@ public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<Exception
             ProblemDetails problemDetails = new()
             {
                 Status = StatusCodes.Status500InternalServerError,
-                Title = $"Server Error: {exception.Message}",
+                Title = $"Что то пошло не так",
             };
 
             context.Response.StatusCode = StatusCodes.Status500InternalServerError;
