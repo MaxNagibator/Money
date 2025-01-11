cc986ea7c1cc0f1be86655d1d7a9f7ad193e681a
Савелий Вотинцев <votintcev1@mail.ru>
2025-01-11 15:24:43 +0300

    Правки BackgroundService
    - Добавлен таймер
    - Добавлено черновое логирование

--- backend/Money.Api/BackgroundServices/RegularTaskBackgroundService.cs
+++ backend/Money.Api/BackgroundServices/RegularTaskBackgroundService.cs
@@ -2,14 +2,29 @@
 
 public class RegularTaskBackgroundService(IServiceProvider serviceProvider, ILogger<RegularTaskBackgroundService> logger) : BackgroundService
 {
+    private readonly PeriodicTimer _timer = new(TimeSpan.FromSeconds(10));
     private DateTime _lastExecuteDate = DateTime.Now.Date.AddDays(-1);
 
-    protected override async Task ExecuteAsync(CancellationToken token)
+    public override Task StopAsync(CancellationToken stoppingToken)
     {
-        while (token.IsCancellationRequested == false)
+        logger.LogInformation("{Name} останавливается", nameof(RegularTaskBackgroundService));
+        _timer.Dispose();
+        return base.StopAsync(stoppingToken);
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        logger.LogInformation("{Name} запускается", nameof(RegularTaskBackgroundService));
+
+        do
         {
+            logger.LogDebug("Проверка необходимости выполнения задачи: "
+                            + "Последняя дата выполнения - {LastExecuteDate}, "
+                            + "Текущая дата - {CurrentDate}", _lastExecuteDate, DateTime.Now.Date);
+
             if (_lastExecuteDate >= DateTime.Now.Date)
             {
+                logger.LogDebug("Задача пропущена, так как уже была выполнена сегодня");
                 continue;
             }
 
@@ -18,17 +33,16 @@ public class RegularTaskBackgroundService(IServiceProvider serviceProvider, ILog
                 await using (var scope = serviceProvider.CreateAsyncScope())
                 {
                     var service = scope.ServiceProvider.GetRequiredService<RegularOperationService>();
-                    await service.RunRegularTaskAsync(token);
+                    await service.RunRegularTaskAsync(stoppingToken);
                 }
 
                 _lastExecuteDate = DateTime.Now.Date;
+                logger.LogDebug("Задача выполнена успешно. Обновлена последняя дата выполнения до {LastExecuteDate}", _lastExecuteDate);
             }
             catch (Exception exception)
             {
                 logger.LogError(exception, "Ошибка при выполнении регулярной задачи");
             }
-
-            await Task.Delay(10000, token);
-        }
+        } while (await _timer.WaitForNextTickAsync(stoppingToken));
     }
 }
