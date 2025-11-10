b96cdf72df9f9ad2da4c51f5687bde663bc78c38
Савелий Вотинцев <votintcev1@mail.ru>
2025-11-11 02:36:52 +0300

    Очистка проекта
    - Удалён Aspire
    - Исправлена плавающая проблема с StaticWebAssets

--- frontend/Money.Web/Program.cs
+++ frontend/Money.Web/Program.cs
@@ -1,9 +1,9 @@
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.ServiceDiscovery;
 using Money.ApiClient;
 using Money.Web.Services.Authentication;
-using Money.WebAssembly.CoreLib;
 using MudBlazor.Services;
 using MudBlazor.Translations;
 using NCalc.DependencyInjection;
@@ -12,7 +12,20 @@ using System.Globalization;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 var apiUri = new Uri($"https://{builder.Configuration["Services:api:https:0"]}");
 
-builder.AddServiceDefaults();
+builder.Services.AddServiceDiscovery();
+
+builder.Services.ConfigureHttpClientDefaults(http =>
+{
+    http.AddStandardResilienceHandler();
+    http.AddServiceDiscovery();
+});
+
+builder.Services.Configure<ConfigurationServiceEndpointProviderOptions>(static options =>
+{
+    options.SectionName = "Services";
+    options.ShouldApplyHostNameMetadata = static _ => true;
+});
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
